32e8e7ff3f80b3f5958180a4add3378ec14dae21
loverikim <loverikim03@gmail.com>
2024-03-29 15:23:36 +0900

    아이콘 이미지 추가, ui 디자인 적용.

--- appTest02.py
+++ appTest02.py
@@ -12,11 +12,15 @@ class MyWindow(QMainWindow, form_class):
         super().__init__() # 부모 클래스 생성자 호출
         self.setupUi(self)  # 위에서 불러온 uitest.ui를 연결
         self.setWindowTitle("연습 프로그램")
+        self.setWindowIcon(QIcon("img/python.png"))
+
         self.button1.clicked.connect(self.button1_click)
         # 버튼1이 클릭되면 button1_click 메서드 호출
 
         self.button2.clicked.connect(self.button2_click)
         # 버튼2이 클릭되면 button2_click 메서드 호출
+
+        self.statusBar().showMessage("made by aerikim 2024-0329") #하단에 글 적힘
 
     def button1_click(self):
         self.label1.setText("Hello World!!")
